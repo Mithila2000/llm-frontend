58f84a71cb4c4238f3dc40b5945699a2166c5c3c
Mithila2000 <mithilasonar17@gmail.com>
2024-11-08 00:08:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Workflow from './Workflow';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Workflow />
+    </div>
+  );
+}
+
+export default App;
